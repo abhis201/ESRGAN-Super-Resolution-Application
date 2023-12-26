7ae36ed1bb4f2083cc4050bd35a0592ec3f50204
singhAbhiPNW <sing1290@pnw.edu>
2023-12-26 15:49:25 -0600

    removed static files

--- ESRWebApp/static/ESRGAN/datasets/TEST/make_patches_main.py
+++
@@ -1,4 +0,0 @@
-from preparation_test.make_patches import make_patches
-
-if __name__ == '__main__':
-    make_patches()
--- ESRWebApp/static/ESRGAN/datasets/prepare_datasets.py
+++
@@ -1,6 +0,0 @@
-from preparation.download_image import download_dataset
-from preparation.make_patches import make_patches
-
-if __name__ == '__main__':
-    download_dataset()
-    make_patches()
